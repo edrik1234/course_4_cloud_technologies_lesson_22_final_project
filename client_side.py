fc4f80ce3aba1f500d7eec48f99540e8a5f1a978
Edrian <razer4832@gmail.com>
2025-08-15 12:11:59 +0300

    we add an automathical section that push all the changes made in file syslog_status file, to git

--- client_side.py
+++ client_side.py
@@ -2,7 +2,8 @@ from SshToServer  import SshToServer
 import pandas as pd
 import os
 import json
-
+import subprocess
+import datetime
 
 def append_to_csv(file_path, data):
     #df_new = pd.DataFrame(data, columns = ("INFO", "WARNING", "ERROR", "DATE FROM 1.1.1970") )
@@ -20,4 +21,10 @@ my_ssh = SshToServer(r"C:\Users\edrik_cgifjkr\Desktop\Course_4_CLOUD_TECHNOLOGY\
 #result, output_error = my_ssh.runRemoteCommand("sudo python3 server_side.py")
 result = my_ssh.result_of_command("sudo python3 server_side.py")
 result = json.loads(result)
-append_to_csv("C:\\Users\\edrik_cgifjkr\\Desktop\\Course_4_CLOUD_TECHNOLOGY\\lesson 22 - final project\\sshtoserver, client_side, server_side before modifying\\syslog_status.csv" , result)
+append_to_csv("C:\\Users\\edrik_cgifjkr\\Desktop\\Course_4_CLOUD_TECHNOLOGY\\lesson 22 - final project\\sshtoserver, client_side, server_side before modifying\\course_4_cloud_technologies_lesson_22_final_project\\syslog_status.csv" , result)
+
+file_path = "syslog_status.csv" 
+commit_message = f"Auto update {file_path} {datetime.datetime.now():%Y-%m-%d %H:%M:%S}"
+subprocess.run(["git", "add", file_path])
+subprocess.run(["git", "commit", "-m", commit_message])
+subprocess.run(["git", "push"])
